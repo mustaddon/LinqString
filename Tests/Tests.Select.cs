c0c5421d105829c3fc43f458b632b20ef3505603
Leonid Salavatov <mustaddon@gmail.com>
2023-12-29 15:15:07 +0300

    tests

--- Tests/Tests.Select.cs
+++ Tests/Tests.Select.cs
@@ -31,14 +31,59 @@ public class TestSelect
             Prop3 = x.Prop3 == null ? null : x.Prop3.Select(xx => xx == null ? null : new { xx.Prop32 }),
         }).ToList();
 
+        // IQueryable
         var cacheless = items.AsQueryable().Select(props).ToList();
         var cachable = items.AsQueryable().Select(props, cache).ToList();
 
         Assert.That(JsonSerializer.Serialize(cacheless), Is.EqualTo(JsonSerializer.Serialize(test)));
         Assert.That(JsonSerializer.Serialize(cachable), Is.EqualTo(JsonSerializer.Serialize(test)));
 
+        // IEnumerable
         cacheless = items.Select(props).ToList();
         cachable = items.Select(props, cache).ToList();
+
+        Assert.That(JsonSerializer.Serialize(cacheless), Is.EqualTo(JsonSerializer.Serialize(test)));
+        Assert.That(JsonSerializer.Serialize(cachable), Is.EqualTo(JsonSerializer.Serialize(test)));
+    }
+
+    [Test]
+    public void Ñascade()
+    {
+        var items = Enumerable.Range(0, 10).Select(x => x == 0 ? null : new
+        {
+            Prop1 = x,
+            Prop2 = x == 1 ? null : new
+            {
+                Prop21 = x,
+                Prop22 = $"text-{x}",
+            },
+            Prop3 = x == 2 ? null : Enumerable.Range(0, 5).Select(xx => xx == 0 ? null : new
+            {
+                Prop31 = xx + x * 1000,
+                Prop32 = $"text-{x}-{xx}",
+            })
+        }).ToList();
+
+        var cache = new NeverExpiredCache();
+        var first = new[] { "Prop1", "Prop2.Prop22", "Prop3.Prop32" };
+        var second = new[] { "Prop1", "Prop2.Prop22" };
+        var third = new[] { "Prop2.Prop22" };
+
+        var test = items.Select(x => x == null ? null : new
+        {
+            Prop2 = x.Prop2 == null ? null : new { x.Prop2.Prop22 },
+        }).ToList();
+
+        // IQueryable
+        var cacheless = items.AsQueryable().Select(first).Select(second).Select(third).ToList();
+        var cachable = items.AsQueryable().Select(first, cache).Select(second, cache).Select(third, cache).ToList();
+
+        Assert.That(JsonSerializer.Serialize(cacheless), Is.EqualTo(JsonSerializer.Serialize(test)));
+        Assert.That(JsonSerializer.Serialize(cachable), Is.EqualTo(JsonSerializer.Serialize(test)));
+
+        // IEnumerable
+        cacheless = items.Select(first).Select(second).Select(third).ToList();
+        cachable = items.Select(first, cache).Select(second, cache).Select(third, cache).ToList();
 
         Assert.That(JsonSerializer.Serialize(cacheless), Is.EqualTo(JsonSerializer.Serialize(test)));
         Assert.That(JsonSerializer.Serialize(cachable), Is.EqualTo(JsonSerializer.Serialize(test)));
